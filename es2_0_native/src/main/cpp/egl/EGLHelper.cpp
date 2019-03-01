f4f19db8d7b3eebfb918bda5da097ffce96f8ee4
ycl <907740329@qq.com>
2019-03-01 17:42:57 +0800

    es2_0 native 创建EGL文件

--- es2_0_native/src/main/cpp/egl/EGLHelper.cpp
+++ es2_0_native/src/main/cpp/egl/EGLHelper.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "EGLHelper.h"
+
+EGLHelper::EGLHelper() {
+
+
+}
+
+EGLHelper::~EGLHelper() {
+
+
+}
+
+int EGLHelper::initEgl(EGLNativeWindowType win) {
+
+    return 0;
+}
+
+int EGLHelper::swapBuffer() {
+
+    return 0;
+}
+
+void EGLHelper::destroyEgl() {
+
+}
--- es2_0_native/src/main/cpp/egl/EGLHelper.h
+++ es2_0_native/src/main/cpp/egl/EGLHelper.h
@@ -5,4 +5,31 @@
 #ifndef OPENGLES_DEMO_EGLHELPER_H
 #define OPENGLES_DEMO_EGLHELPER_H
 
+
+#include <EGL/egl.h>
+
+class EGLHelper {
+
+public:
+
+    EGLDisplay eglDisplay;
+    EGLSurface eglSurface;
+    EGLConfig eglConfig;
+    EGLContext eglContext;
+
+
+public:
+
+    EGLHelper();
+
+    virtual ~EGLHelper();
+
+    int initEgl(EGLNativeWindowType win);
+
+    int swapBuffer();
+
+    void destroyEgl();
+
+};
+
 #endif //OPENGLES_DEMO_EGLHELPER_H
